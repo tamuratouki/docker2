292c8372f229dcf0eacbc34b86627122f5fd960f
“tamuratouki” <touki9480@icloud.com>
2021-02-14 12:48:59 +0900

    新規作成

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,44 +8,45 @@ use Illuminate\Http\RedirectResponse;
 
 class CustomerController extends Controller
 {
-public function index():View
-{
-    return view('customer.index');
-//初期表示
-}
-public function　search (Request $request):View
-{
-    return view('customer.index');
-    //検索
-}
-public function create(Request $request) : View
-{
-    return view('customer.create');
-    //新規登録画面の表示
-}
-public function detail(Request $request): View
- {
-  //詳細画面の表示
-}
-public function edit(Request $request) : View
-{
-    // 編集画面の表示
-}
-public function store(Request $request):RedirectResponse
-{
-    return redirect()->(route:'customer_index');
-    //新規登録画面の表示
-}
-public function updata(Request $request):RedirectResponse 
-{
-    return redirect()->(route:'customer_index');
-    //編集画面で更新
-}
-public function delete(Request $request):RedirectResponse 
-{
-    return redirect()->(route:'customer_index');
-    //詳細画面で削除
-}
+    /*
+     * Undocumented function
+     *
+     * @return View
+     */
+    public function index():View
+    {
+        return view('customer.index');
+    }
+        public function search (Request $request):View
+            {
+                return view('customer.index');
+            }
+
+                public function create(Request $request) : View
+                    {
+                        return view('customer.create');
+                    }
+
+                        public function detail(Request $request): View
+                            {
+
+                            }
+                            public function edit(Request $request) : View
+                                {
+
+                                }
+                                public function store(Request $request):RedirectResponse
+                                {
+                                    return redirect()->('customer_index');
+                                }
+                                    public function updata(Request $request):RedirectResponse
+                                    {
+                                        return redirect()->('customer_index');
+                                    }
+                                        public function delete(Request $request):RedirectResponse
+                                        {
+                                            return redirect()->('customer_index');
+                                        }
 
 
 
@@ -55,5 +56,5 @@ public function delete(Request $request):RedirectResponse
 
 
 
-{
+
 }
